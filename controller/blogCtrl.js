45f0859851ac1857a8fc70f34d8e7d86bb303b7b
NgocChau0403 <21521885@gm.uit.edu.vn>
2024-12-02 14:08:36 +0700

    Create/Update/Get/Delete/Like/Dislike Blogs

---
+++ controller/blogCtrl.js
@@ -0,0 +1,186 @@
+const Blog = require("../models/blogModel");
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
+const createBlog = asyncHandler(async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.json(newBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateBlog = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  validateMongoDbId(id);
+  try {
+    const updateBlog = await Blog.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    res.json(updateBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getBlog = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  validateMongoDbId(id);
+  try {
+    const getBlog = await Blog.findById({ _id: id })
+      .populate("likes")
+      .populate("dislikes");
+    const updateViews = await Blog.findByIdAndUpdate(
+      { _id: id },
+      {
+        $inc: { numViews: 1 },
+      },
+      { new: true }
+    );
+    res.json(getBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllBlogs = asyncHandler(async (req, res) => {
+  try {
+    const getBlogs = await Blog.find();
+    res.json(getBlogs);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteBlog = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  validateMongoDbId(id);
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete({ _id: id });
+    res.json(deleteBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const likeBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.body;
+  validateMongoDbId(blogId);
+
+  // Lấy thông tin người dùng từ req.user (đã được gán trong middleware authMiddleware)
+  const userId = req.user._id;
+
+  // Tìm blog mà người dùng muốn like
+  const blog = await Blog.findById(blogId);
+  // Kiểm tra xem người dùng đã like blog này chưa
+  const isLiked = blog?.isLiked;
+
+  // Kiểm tra xem người dùng đã dislike blog này chưa
+  const alreadyDisliked = blog?.dislikes?.find(
+    (userId) => userId?.toString() === userId.toString()
+  );
+
+  // Nếu người dùng đã dislike blog, bỏ dislike
+  if (alreadyDisliked) {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: userId },
+        isDisliked: false,
+      },
+      { new: true }
+    );
+    return res.json(updatedBlog);
+  }
+
+  if (isLiked) {
+    // Nếu đã like, bỏ like
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: userId },
+        isLiked: false,
+      },
+      { new: true }
+    );
+    return res.json(updatedBlog);
+  } else {
+    // Nếu chưa like và chưa dislike, dislike blog
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { likes: userId },
+        isLiked: true,
+      },
+      { new: true }
+    );
+    return res.json(updatedBlog);
+  }
+});
+
+const dislikeBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.body;
+  validateMongoDbId(blogId);
+
+  // Lấy thông tin người dùng từ req.user (đã được gán trong middleware authMiddleware)
+  const userId = req.user._id;
+
+  // Tìm blog mà người dùng muốn like
+  const blog = await Blog.findById(blogId);
+
+  // Kiểm tra xem người dùng đã like blog này chưa
+  const isDisliked = blog?.isDisliked;
+  // Kiểm tra xem người dùng đã dislike blog này chưa
+  const alreadyLiked = blog?.likes?.find(
+    (userId) => userId?.toString() === userId.toString()
+  );
+
+  // Nếu người dùng đã dislike blog, bỏ dislike
+  if (alreadyLiked) {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: userId },
+        isLiked: false,
+      },
+      { new: true }
+    );
+    return res.json(updatedBlog);
+  }
+
+  if (isDisliked) {
+    // Nếu đã like, bỏ like
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: userId },
+        isDisliked: false,
+      },
+      { new: true }
+    );
+    return res.json(updatedBlog);
+  } else {
+    // Nếu chưa like và chưa dislike, dislike blog
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { dislikes: userId },
+        isDisliked: true,
+      },
+      { new: true }
+    );
+    return res.json(updatedBlog);
+  }
+});
+
+module.exports = {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getAllBlogs,
+  deleteBlog,
+  likeBlog,
+  dislikeBlog,
+};
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,28 +2,42 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
+// Middleware xác thực người dùng
 const authMiddleware = asyncHandler(async (req, res, next) => {
   let token;
   if (req?.headers?.authorization?.startsWith("Bearer")) {
     token = req.headers.authorization.split(" ")[1];
     try {
       if (token) {
+        // Giải mã token và lấy thông tin người dùng
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(decoded?.id);
-        req.user = user;
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        // Gán thông tin người dùng vào req.user
+        req.user = {
+          _id: decoded.id,
+          email: user.email,
+          role: user.role, // Gán cả role nếu cần
+        };
         next();
+      } else {
+        throw new Error("No token attached to header");
       }
     } catch (error) {
-      throw new Error("Not Authorized token expired, Please Login again");
+      throw new Error("Not Authorized or Token expired, Please Login again");
     }
   } else {
-    throw new Error(" There is no token attached to header");
+    throw new Error("No token attached to header");
   }
 });
+
+// Middleware kiểm tra quyền Admin
 const isAdmin = asyncHandler(async (req, res, next) => {
-  const { email } = req.user;
-  const adminUser = await User.findOne({ email });
-  if (adminUser.role !== "admin") {
+  const { role } = req.user;
+  if (role !== "admin") {
     throw new Error("You are not an admin");
   } else {
     next();
